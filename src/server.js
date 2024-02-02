ee75a87a172c0af1f109259e578f33aab40a88bf
Eduardo Tavares <eduardotdev.21@gmail.com>
2024-02-02 03:16:07 -0300

    rotas de criacao e listagem - metodos http

--- src/server.js
+++ src/server.js
@@ -1,6 +1,32 @@
 import http from 'node:http'
 
+/*
+  - HTTP
+    - Método HTTP:
+      GET => buscar um recurso no back-end
+      POST => criar um recurso no back-end
+      PUT => Editar ou atualizar um recurso no back-end
+      PATCH => Atualizar uma informação ÚNICA ou ESPECÍFICA de um recurso no back-end
+      DELETE => Deletar um recurso do back-end
+
+    - URL
+
+  GET /users => Buscar usuários no back-end
+  POST /users => Criar um usuário no back-end
+*/
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    // Early return
+    return res.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' & url === '/users') {
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Hello World')
 })
 

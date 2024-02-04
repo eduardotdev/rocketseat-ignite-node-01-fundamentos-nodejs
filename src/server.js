9ba790e0f24e9974a260bcf3f1e4cd46e3865e80
Eduardo Tavares <eduardotdev.21@gmail.com>
2024-02-04 15:57:18 -0300

    conhecendo http status codes

--- src/server.js
+++ src/server.js
@@ -21,6 +21,8 @@ import http from 'node:http'
 
 // Cabeçalhos (Requisição/Resposta) => Metadados
 
+// HTTP Status Code
+
 const users = []
 
 const server = http.createServer((req, res) => {
@@ -40,10 +42,10 @@ const server = http.createServer((req, res) => {
       email: 'johndoe@example.com'
     })
 
-    return res.end('Criação de usuário')
+    return res.writeHead(201).end()
   }
 
-  return res.end('Hello World')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
